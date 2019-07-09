93c68f3db4b412c560e938f1a094edb4a5688abc
Vadim <zex404@gmail.com>
2019-07-09 19:56:52 +1000

    v1.0.1

--- functions.php
+++ functions.php
@@ -11,41 +11,37 @@ class Unit {
     ];
 
     protected static $args = [
+
 	    'label' => 'Unit',
-	    'labels' => [
-		    'name' => 'Units',
-		    'singular_name' => 'Unit',
-		    'menu_name' => 'Unit',
-		    'name_admin_bar' => 'Unit'
-	    ],
-	    'supports' => [
-		    'editor',
-		    'title'
-	    ],
-	    'register_meta_box_cb' => [__CLASS__, 'register_metabox'],
+	    'labels' => ['name' => 'Units', 'singular_name' => 'Unit', 'menu_name' => 'Unit', 'name_admin_bar' => 'Unit'],
+
+	    'supports' => ['title', 'editor'],
+
+        'public' => TRUE,
+        'publicly_queryable' => TRUE,
+        'exclude_from_search' => FALSE,
+
 	    'hierarchical' => FALSE,
-	    'public' => TRUE,
+        'can_export' => TRUE,
+        'has_archive' => FALSE,
+
 	    'show_ui' => TRUE,
 	    'show_in_menu' => TRUE,
+        'show_in_admin_bar' => TRUE,
+        'show_in_nav_menus' => TRUE,
+        'show_in_rest' => FALSE,
+
 	    'menu_position' => 1,
 	    'menu_icon' => 'dashicons-format-gallery',
-	    'show_in_admin_bar' => TRUE,
-	    'show_in_nav_menus' => TRUE,
-	    'can_export' => TRUE,
-	    'has_archive' => FALSE,
-	    'exclude_from_search' => FALSE,
-	    'publicly_queryable' => TRUE,
-	    'rewrite' => [
-		    'slug' => 'unit',
-		    'with_front' => TRUE,
-		    'pages' => FALSE,
-		    'feeds' => FALSE,
-	    ],
+
+	    'rewrite' => ['slug' => 'unit', 'with_front' => TRUE, 'pages' => FALSE, 'feeds' => FALSE],
+
 	    'capability_type' => 'post',
-	    'show_in_rest' => FALSE
+
+        'register_meta_box_cb' => [__CLASS__, 'register_metabox']
     ];
 
-    public static function init (array $args = NULL) : void {
+    public static function init (array $args = NULL) {
 
 	    if ($args !== NULL)
 
@@ -58,19 +54,19 @@ class Unit {
 	    \add_shortcode ('yandex_map', [__CLASS__, 'shortcode_yandex_map']);
     }
 
-    public static function register_type () : void {
+    public static function register_type () {
 
 	    if (\is_wp_error ($error = \register_post_type ('unit', static::$args)))
 
 	        throw new \Exception ($error->get_error_message (), $error->get_error_code ());
     }
 
-    public static function register_metabox (WP_Post $post) : void {
+    public static function register_metabox (WP_Post $post) {
 
 	    \add_meta_box ('lola', 'Coordinates', [__CLASS__, 'render_metabox'], 'unit', 'normal', 'high');
 	}
 
-	public static function render_metabox () : void {
+	public static function render_metabox () {
 
 		$post = static::unit_with_meta ();
 
@@ -82,7 +78,7 @@ class Unit {
 		<?php endif;
 	}
 
-	public static function save_post () : void {
+	public static function save_post () {
 
 		if (empty ($_POST)) return;
 
@@ -106,7 +102,7 @@ class Unit {
 	        \wp_register_script ('ym.api', str_replace (['%locale', '%key'], [\get_locale (), static::API_KEY], static::API_URL));
 	        \wp_register_script ('ym.app', \get_stylesheet_directory_uri () . '/ym.js');
 
-	        \wp_localize_script ('ym.app', 'ym', ['ajax_url'   => \admin_url( 'admin-ajax.php' ), 'unit' => $post]);
+	        \wp_localize_script ('ym.app', 'ym', ['unit' => $post]);
 
 	        \wp_enqueue_script ('ym.api');
 	        \wp_enqueue_script ('ym.app');
@@ -115,7 +111,7 @@ class Unit {
 		\wp_enqueue_style ('ym.style', \get_template_directory_uri () . '/style.css' );
     }
 
-    public static function unit_with_meta () {
+    public static function unit_with_meta () : WP_Post {
 
 	    global $post;
 
